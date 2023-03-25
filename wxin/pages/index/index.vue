d77e9b230334877a680a9e11ff67ca2f41eb7bf3
yangsen996 <838725628@qq.com>
2023-03-25 09:44:34 +0800

    'first'

---
+++ wxin/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class="container">
+		{{msg}}
+	</view>
+</template>
+
+<script setup>
+import { ref } from "vue";
+	const msg = ref("test")
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
